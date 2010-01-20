e64bab3ab043f1c32e3cafbabdc37285a1aa7772
Elias Baixas <elias@eresmas.net>
2010-01-20 20:39:43 +0100

    Fixed stuff with implementing Daemon interface

--- src/common/policyserver/PolicyServerDaemon.java
+++ src/common/policyserver/PolicyServerDaemon.java
@@ -7,7 +7,7 @@ import org.apache.commons.daemon.DaemonContext;
 import org.apache.log4j.Logger;
 
 /**
- * void load(String[] arguments): Here open the configuration files, create the trace file, create the ServerSockets, the Threads
+ * void init(context): Here open the configuration files, create the trace file, create the ServerSockets, the Threads
  * void start(): Start the Thread, accept incoming connections
  * void stop(): Inform the Thread to live the run(), close the ServerSockets
  * void destroy(): Destroy any object created in init()
@@ -15,43 +15,44 @@ import org.apache.log4j.Logger;
  * @author elias
  *
  */
-public class PolicyServerDaemon {
+public class PolicyServerDaemon implements Daemon{
 	
 	private static final Logger log = Logger.getLogger(PolicyServerDaemon.class);
 
 	private PolicyServer policyServer;
 
 	/**
-	 * Here open the configuration files, create the trace file, create the ServerSockets, the Threads
-	 * @param arguments
-	 */
-	void load(String[] arguments){
-		policyServer = new PolicyServer();
-		try {
-			policyServer.startListening();
-		} catch (IOException e) {
-			log.error(e.getMessage(),e);
-		}
-	}
-	
-	/**
 	 * Start the Thread, accept incoming connections
 	 */
-	void start(){
+	public void start(){
 		policyServer.start();
 	}
 	
 	/**
 	 * Inform the Thread to live the run(), close the ServerSockets
 	 */
-	void stop(){
+	public void stop(){
 		policyServer.stop();
 	}
 
 	/**
 	 * Destroy any object created in init()
 	 */
-	void destroy(){
+	public void destroy(){
 		
+	}
+
+	/**
+	 * Here open the configuration files, create the trace file, create the ServerSockets, the Threads
+	 * @param arguments
+	 */
+	@Override
+	public void init(DaemonContext arg0) throws Exception {
+		policyServer = new PolicyServer();
+		try {
+			policyServer.startListening();
+		} catch (IOException e) {
+			log.error(e.getMessage(),e);
+		}
 	}
 }
